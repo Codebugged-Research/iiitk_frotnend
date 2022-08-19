02fa5f042d276ff8324c73e3c4fa335291d09564
mastergokul <selvarajgokul09@gmail.com>
2022-08-19 10:13:35 +0530

    footer update

--- src/examples/Footers/DefaultFooter/index.js
+++ src/examples/Footers/DefaultFooter/index.js
@@ -28,16 +28,22 @@ import MKBox from "components/MKBox";
 import MKTypography from "components/MKTypography";
 
 function DefaultFooter({ content }) {
-  const { brand, socials, menus, copyright } = content;
+  const { brand, socials, menus } = content;
 
   return (
     <MKBox component="footer">
       <Container>
-        <Grid container spacing={3}>
-          <Grid item xs={12} md={3} sx={{ ml: "auto", mb: 3 }}>
+        <Grid container spacing={3} mb={10}>
+          <Grid item xs={12} md={7} sx={{ ml: "auto", mb: 3 }}>
             <MKBox>
               <Link to={brand.route}>
-                <MKBox component="img" src={brand.image} alt={brand.name} maxWidth="2rem" mb={2} />
+                <MKBox
+                  component="img"
+                  src={brand.image}
+                  alt={brand.name}
+                  maxWidth="2.5rem"
+                  mb={2}
+                />
               </Link>
               <MKTypography variant="h6">{brand.name}</MKTypography>
             </MKBox>
@@ -60,7 +66,7 @@ function DefaultFooter({ content }) {
             </MKBox>
           </Grid>
           {menus.map(({ name: title, items }) => (
-            <Grid key={title} item xs={6} md={2} sx={{ mb: 3 }}>
+            <Grid key={title} item xs={8} md={2.5} sx={{ mb: 3 }}>
               <MKTypography
                 display="block"
                 variant="button"
@@ -101,9 +107,9 @@ function DefaultFooter({ content }) {
               </MKBox>
             </Grid>
           ))}
-          <Grid item xs={12} sx={{ textAlign: "center", my: 3 }}>
+          {/* <Grid item xs={12} sx={{ textAlign: "center", my: 3 }}>
             {copyright}
-          </Grid>
+          </Grid> */}
         </Grid>
       </Container>
     </MKBox>
--- src/footer.routes.js
+++ src/footer.routes.js
@@ -40,35 +40,20 @@ export default {
     {
       name: "company",
       items: [
-        { name: "about us", href: "https://www.creative-tim.com/presentation" },
-        { name: "freebies", href: "https://www.creative-tim.com/templates/free" },
-        { name: "premium tools", href: "https://www.creative-tim.com/templates/premium" },
-        { name: "blog", href: "https://www.creative-tim.com/blog" },
+        { name: "about us", href: "/about" },
+        { name: "freebies", href: "#" },
+        { name: "premium tools", href: "#" },
+        { name: "blog", href: "#" },
       ],
     },
     {
-      name: "resources",
+      name: "contact us",
       items: [
-        { name: "illustrations", href: "https://iradesign.io/" },
-        { name: "bits & snippets", href: "https://www.creative-tim.com/bits" },
-        { name: "affiliate program", href: "https://www.creative-tim.com/affiliates/new" },
-      ],
-    },
-    {
-      name: "help & support",
-      items: [
-        { name: "contact us", href: "https://www.creative-tim.com/contact-us" },
-        { name: "knowledge center", href: "https://www.creative-tim.com/knowledge-center" },
-        { name: "custom development", href: "https://services.creative-tim.com/" },
-        { name: "sponsorships", href: "https://www.creative-tim.com/sponsorships" },
-      ],
-    },
-    {
-      name: "legal",
-      items: [
-        { name: "terms & conditions", href: "https://www.creative-tim.com/terms" },
-        { name: "privacy policy", href: "https://www.creative-tim.com/privacy" },
-        { name: "licenses (EULA)", href: "https://www.creative-tim.com/license" },
+        { name: "Dr. Bharat Lohani", href: "mailto:blohani@iitk.ac.in" },
+        { name: "Associate Professor", href: "mailto:blohani@iitk.ac.in" },
+        { name: "Department of Civil Engineering", href: "mailto:blohani@iitk.ac.in" },
+        { name: "Indian Institute of Technology Kanpur", href: "mailto:blohani@iitk.ac.in" },
+        { name: "Kanpur-208016, INDIA.", href: "mailto:blohani@iitk.ac.in" },
       ],
     },
   ],
